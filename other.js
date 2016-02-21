8a3458f6f57d2ae1fadb369491b1fbbb42da7471
meij <1193413562@qq.com>
2016-02-21 11:09:30 +0800

    modify other.js

--- other.js
+++ other.js
@@ -1,35 +1,33 @@
 // 千位分隔符
 function thousandSeparator(num) {
-    var numStr = num.toString();
+    var numStr = num.toString(),
+        minus = false;
 
     if (numStr.indexOf('-') > -1) {
         numStr = numStr.substring(1);
+        minus = true;
     }
     var numLen = numStr.length,
-        count = Math.floor(numLen / 3),
-        numArr = [];
-
+        count = Math.floor(numLen / 3);
     if (numLen === 3) {
         return num;
-    }
-
-    // 消除位数为3的倍数且大于3的情况
-    if (numLen % 3 === 0) {
-        count--;
-    }
-    // 获得最后三位并插入numArr
-    while (count--) {
+    } else {
         num = num / 1000;
-        casArr = num.toString().split('.');
-        numArr.push(casArr[1]);
-        num = parseInt(num);
-        if (count == 0) {
-            numArr.push(casArr[0]);
-        }
+        numstr = (num + '').replace('.', ',');
     }
 
-    // 规划分组数字顺序
-    num = numArr.reverse().join(',');
-    // console.log(num);
+    return numstr;
+}
+// 正则实现
+function commafy(num) {
+    num = num + '';
+    var reg = /(-?\d+)(\d{3})/;
+
+    if (reg.test(num)) {
+        num = num.replace(reg, '$1,$2');
+    }
+
     return num;
 }
+// commafy(123456789);
+thousandSeparator(-123456789);
