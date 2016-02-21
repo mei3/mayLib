4f8bc560bfcba2c355ff8f0d71a7b5fdc3e02792
meij <1193413562@qq.com>
2016-02-21 15:53:27 +0800

    add a http stream commet

---
+++ mayAjax.js
@@ -0,0 +1,25 @@
+// http流实现comet
+function createStreamingClient(url, progress, finished) {
+    var xhr = new XMLHttpRequest(),
+        received = 0;
+    xhr.open("get", url, true);
+    xhr.onreadystatechange = function() {
+        var result;
+        if (xhr.readyState == 3) {
+            //只取得最新数据并调整计数器
+            result = xhr.responseText.substring(received);
+            received += result.length;
+            //调用 progress 回调函数
+            progress(result);
+        } else if (xhr.readyState == 4) {
+            finished(xhr.responseText);
+        }
+    };
+    xhr.send(null);
+    return xhr;
+}
+var client = createStreamingClient("streaming.php", function(data) {
+    alert("Received: " + data);
+}, function(data) {
+    alert("Done!");
+});
