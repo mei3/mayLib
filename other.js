842cf5d979b6e7af8e607514165c4c4deaf5f7f2
meij <1193413562@qq.com>
2016-02-22 11:00:19 +0800

    add cookie funtion

--- other.js
+++ other.js
@@ -1,4 +1,4 @@
-// 第一种
+// 第一种实现千位分隔符
 function thousandSeparator(num) {
     var numStr = num + '';
 
@@ -17,7 +17,7 @@ function thousandSeparator(num) {
     }
     return numstr;
 }
-// 正则实现
+// 正则实现前卫分隔符
 function commafy(num) {
     num = num + '';
     var reg = /(-?\d+)(\d{3})/;
@@ -27,4 +27,61 @@ function commafy(num) {
     }
 
     return num;
+}
+
+
+
+/**
+ * 设置Cookie （通用函数）
+ * @param {string} cookieName Cookie 名称
+ * @param {string} cookieValue Cookie 值
+ * @param {string} nDays Cookie 过期天数
+ */
+function SetCookie(cookieName, cookieValue, nDays) {
+
+    /* 当前日期 */
+    var today = new Date();
+    /* Cookie 过期时间 */
+    var expire = new Date();
+    /* 如果未设置nDays参数或者nDays为0，取默认值 1 */
+    if (nDays == null || nDays == 0)
+        nDays = 1;
+    /* 计算Cookie过期时间 */
+    expire.setTime(today.getTime() + nDays * 24 * 60 * 60 * 1000); //获取毫秒数
+    /* 设置Cookie值 */
+    document.cookie = cookieName + "=" + escape(cookieValue) + ";expires=" + expire.toGMTString();
+}
+
+
+/**
+ * 读取指定的Cookie值
+ * @param {string} cookieName Cookie名称
+ */
+
+// 方法1：
+
+function GetCookie(cookieName) {
+    var strCookie = document.cookie;
+    // 将多cookie切割为多个名/值对
+    var arrCookie = strCookie.split("; ");
+    for (var i = 0; i < arrCookie.length; i++) {
+        // 遍历cookie数组，处理每个cookie对
+        var arr = arrCookie[i].split("=");
+        // 找到名称为cookieName的cookie，并返回它的值
+        if (arr[0] == "username")
+            return arr[1];
+    }
+    return "";
+
+}
+/**删除cookie
+ *@param {string} cookie的名称
+ */
+function deleteCookie(username) {
+    var date = new Date();
+    // // 删除一个cookie，就是将其过期时间设定为一个过去的时间
+    date.setTime(date.getTime() - 10000);
+
+    document.cookie = name + "=v; expire=" + date.toGMTString();
+
 }
